9a0deb9b680f8a19e1a436ede959c7a8bbcaa96a
FaizenXf <majiburfreefire@gmail.com>
2024-08-22 08:21:42 +0000

    Express

--- bot.js
+++ bot.js
@@ -13,6 +13,19 @@ const { validateConfiguration } = require("@helpers/Validator");
 
 validateConfiguration();
 
+//express run
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
 // initialize client
 const client = new BotClient();
 client.loadCommands("src/commands");
